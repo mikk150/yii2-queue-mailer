5176f4664c2a7aa402b2df75688fbbde5773085b
Dmitry Alekseev <mail@alexeevdv.ru>
2018-07-14 12:46:36 +0300

    Updated yii2-queue package

--- src/jobs/MailJob.php
+++ src/jobs/MailJob.php
@@ -2,26 +2,45 @@
 
 namespace mikk150\queuemailer\jobs;
 
-use yii\queue\ActiveJob;
+use yii\base\BaseObject;
+use yii\base\InvalidConfigException;
 use yii\di\Instance;
+use yii\mail\MailerInterface;
+use yii\mail\MessageInterface;
+use yii\queue\JobInterface;
 
 /**
-*
-*/
-class MailJob extends ActiveJob
+ * Class MailJob
+ * @package mikk150\queuemailer\jobs
+ */
+class MailJob extends BaseObject implements JobInterface
 {
+    /**
+     * @var string|array|MailerInterface
+     */
     public $mailer;
 
+    /**
+     * @var string|array|MessageInterface
+     */
     public $message;
 
-    public function queueName()
+    /**
+     * @inheritdoc
+     * @throws InvalidConfigException
+     */
+    public function init()
     {
-        return 'mailjob';
+        parent::init();
+        $this->mailer = Instance::ensure($this->mailer, MailerInterface::class);
+        $this->message = Instance::ensure($this->message, MessageInterface::class);
     }
 
-    public function run()
+    /**
+     * @inheritdoc
+     */
+    public function execute($queue)
     {
-        $mailer = Instance::ensure($this->mailer, 'yii\mail\BaseMailer');
-        $mailer->send($this->message);
+        $this->mailer->send($this->message);
     }
 }
